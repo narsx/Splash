3ee561f571bca1847afc984bc5ac058e1a7fca46
Fernando <Fernando@DESKTOP-6ANQHFG>
2018-12-06 21:46:32 +0100

    c

---
+++ SplashScreenFer/src/application/Controller.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSpinner;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Controller {
+	
+	
+	
+}
